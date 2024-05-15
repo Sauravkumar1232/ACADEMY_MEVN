cb899a6605efd82c2a9438cd40f516a6a81e8f11
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-05-15 10:05:49 +0530

    add dashboard

--- admin-project/src/routes.js
+++ admin-project/src/routes.js
@@ -1,5 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "./views/Dashboard.vue";
+// import Vue from "vue";
+// import VueRouter from "vue-router";
+// import dashboard from "../views/dashboard";
 const routes = [
+  {
+    path: "",
+    component: Dashboard,
+    children: [
+      {
+        path: "",
+        component: () =>
+          import(/* webpackChunkName: "Overview" */ "./views/OverView.vue"),
+      },
+      {
+        path: "messages",
+        component: () =>
+          import(/* webpackChunkName: "Messages" */ "./views/MessAges.vue"),
+      },
+      {
+        path: "profile",
+        component: () =>
+          import(/* webpackChunkName: "Profile" */ "./views/ProFile.vue"),
+      },
+      {
+        path: "settings",
+        component: () =>
+          import(/* webpackChunkName: "Settings" */ "./views/SettIngs.vue"),
+      },
+    ],
+  },
+
   {
     path: "/course/Create",
     name: "CourseCreate",
@@ -16,7 +47,7 @@ const routes = [
     component: () => import("./components/Course/CourseList"),
   },
   {
-    path: "/",
+    path: "/dashboard",
     name: "CourseEx",
     component: () => import("./components/Course/CourseEx"),
   },
--- api/router/BranchRouter.js
+++ api/router/BranchRouter.js
@@ -8,4 +8,14 @@ router.post("/branch/create", (req, res) => {
 router.get("/branch/list", (req, res) => {
   BranchController.getAllBranch(req, res);
 });
+
+router.delete("/branch/delete/:id", (req, res) => {
+  BranchController.deleteBranch(req, res);
+});
+router.get("/branch/forEdit/:id", (req, res) => {
+  BranchController.getBranchForEdit(req, res);
+});
+router.put("/branch/edit/:id", (req, res) => {
+  BranchController.branchEdit(req, res);
+});
 module.exports = router;
